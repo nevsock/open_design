be0a860c9ba38d5959ca1ab3e67adbda4b70f5ff
Alex <alex.zzh.s7@gmail.com>
2019-01-09 12:22:13 +0900

    Edit timer2

--- timer2.js
+++ timer2.js
@@ -1,4 +1,5 @@
 //https://iwb.jp/easy-javascript-countdown-timer/
+var inte = 2000;
 function timer() {
     var stopped = false;
 
@@ -9,7 +10,8 @@ function timer() {
     var DAYEND = new Date(`${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()} ${end}`);
     var INTERVAL = 1000;
     var calc = new Date(+DAYEND - DAYSTART + INTERVAL);
-
+    const c = calc;
+    mainFunction();
     function countdownTimer() {
         var addZero = function(n) { return ('0' + n).slice(-2); }
         if (+new Date(calc) <= INTERVAL) {
@@ -21,6 +23,7 @@ function timer() {
             var minutes = addZero(calc.getUTCMinutes()) ;
             var seconds = addZero(calc.getUTCSeconds()) ;
             document.getElementById('result').innerHTML = date + '日' + hours + '時間' + minutes + '分' + seconds + '秒';
+            inte = calc / c * 2000;
         }
     }
     setInterval(function() { 
@@ -33,4 +36,22 @@ function timer() {
 
 function stop(){
     stopped = true;
+}
+
+async function mainFunction() {
+    var gpioAccess = await navigator.requestGPIOAccess(); 
+    var port = gpioAccess.ports.get(26);
+    await port.export("out");
+    var v = 0;
+    while (true) {
+      await sleep(inte);
+      v ^= 1;
+      port.write(v);
+    }
+}
+  
+function sleep(ms) {
+    return new Promise(function(resolve) {
+      setTimeout(resolve, ms);
+    });
 }
